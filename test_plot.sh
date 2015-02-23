987f7843235e92b26cf22f379d870ebbff22a5cb
Russell <russell.s.stewart@gmail.com>
2015-02-23 12:39:30 -0800

    Updated test_plot.sh and train_plot.sh.

--- test_plot.sh
+++ test_plot.sh
@@ -1,6 +1,3 @@
-pyplt() {
-  py -c 'matplotlib.use(`Agg`); from matplotlib import pyplot as plt' -C 'plt.savefig("output.jpg")'
-}
 while sleep 10; do
-    cat a.txt | grep test | grep category | py --si 'loss = ' 'x[1][:4]' -x | pyplt --ji -l 'plt.plot(l)'
+    cat a.txt | grep Test | grep category | py --si 'loss = ' 'x[1][:4]' -x | py -c 'matplotlib.use(`Agg`); from matplotlib import pyplot as plt' -C 'plt.savefig("img_test.jpg")' --ji -l 'plt.plot(l)' 2>&1 > /dev/null
 done
--- train_plot.sh
+++ train_plot.sh
@@ -1,6 +1,3 @@
-pyplt() {
-  py -c 'matplotlib.use(`Agg`); from matplotlib import pyplot as plt' -C 'plt.savefig("output.jpg")'
-}
 while sleep 10; do
-    cat a.txt | grep Iteration | grep loss | py --si 'loss = ' 'x[1][:4]' -x | py -l l[10:] | py --ji -l 'numpy.convolve(numpy.ones(100)/100, l)' | py --ji -l l[100:-100] | pyplt --ji -l 'plt.plot(l)'
+    cat a.txt | grep Iteration | grep loss | py --si 'loss = ' 'x[1][:4]' -x | py -l l[10:] | py --ji -l 'numpy.convolve(numpy.ones(100)/100, l)' | py --ji -l l[100:-100] | py -c 'matplotlib.use(`Agg`); from matplotlib import pyplot as plt' -C 'plt.savefig("img_train.jpg")' --ji -l 'plt.plot(l)' 2>&1 > /dev/null
 done
