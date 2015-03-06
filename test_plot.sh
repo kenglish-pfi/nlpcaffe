50aed0dbb8083ebfe5f39a1304fe2340381ef556
Russell <russell.s.stewart@gmail.com>
2015-03-05 23:54:53 -0800

    Updated test_plot and train_plot.

--- test_plot.sh
+++ test_plot.sh
@@ -1,3 +1,8 @@
 while sleep 10; do
-    cat a.txt | grep Test | grep category | py --si 'loss = ' 'x[1][:4]' -x | py -c 'matplotlib.use(`Agg`); from matplotlib import pyplot as plt' -C 'plt.savefig("img_test.jpg")' --ji -l 'plt.plot(l)' 2>&1 > /dev/null
+    for d in `find .. -maxdepth 1 -type d -name '*caffe*'`; do
+        cd $d
+        cd ../caffe$i
+        cat a.txt | grep Test | grep -v Iteration | py --si 'loss = ' 'x[1][:4]' -x | py -l --ji 'numpy.array(l[::2]) + numpy.array(l[1::2])' | py -c 'matplotlib.use(`Agg`); from matplotlib import pyplot as plt' -C 'plt.savefig("img_test.jpg")' --ji -l 'plt.plot(l)' 2>&1 > /dev/null
+    done
 done
+while sleep 10; do
