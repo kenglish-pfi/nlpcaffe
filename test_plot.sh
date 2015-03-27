5509c476a0cf7f7d7dbd17f1446fa68df6ba2d0c
Russell <russell.s.stewart@gmail.com>
2015-03-27 16:08:47 -0700

    Removed unrelated files from root dir.

--- test_plot.sh
+++
@@ -1,7 +0,0 @@
-while sleep 10; do
-    for d in `find .. -maxdepth 1 -type d -name '*caffe*'`; do
-        cd $d
-        cat a.txt | grep Test | grep -v Iteration | py --si 'loss = ' 'x[1][:4]' -x | py -l --ji 'numpy.array(l[::2]) + numpy.array(l[1::2])' | py -c 'matplotlib.use(`Agg`); from matplotlib import pyplot as plt' -C 'plt.savefig("img_test.jpg")' --ji -l 'plt.plot(l)' 2>&1 > /dev/null
-    done
-done
-while sleep 10; do
--- train_plot.sh
+++
@@ -1,6 +0,0 @@
-while sleep 10; do
-    for d in `find .. -maxdepth 1 -type d -name '*caffe*'`; do
-        cd $d
-        cat a.txt | grep Iteration | grep loss | py --si 'loss = ' 'x[1][:4]' -x | py -l l[10:] | py --ji -l 'numpy.convolve(numpy.ones(100)/100, l)' | py --ji -l l[100:-100] | py -c 'matplotlib.use(`Agg`); from matplotlib import pyplot as plt' -C 'plt.savefig("img_train.jpg")' --ji -l 'plt.plot(l)' 2>&1 > /dev/null
-    done
-done
