54c0c6a45049c9c9fc60b0bfa151f0cb153e2537
kenglish-pfi <kenglish.pfi@gmail.com>
2017-01-27 16:10:51 -0600

    added apt-get update

--- docker/standalone/gpu/Ubuntu14.04-AMI/step_0-configure_gpu.sh
+++ docker/standalone/gpu/Ubuntu14.04-AMI/step_0-configure_gpu.sh
@@ -12,9 +12,12 @@ done
 # Reference:
 #   https://github.com/NVIDIA/nvidia-docker/wiki/Deploy-on-Amazon-EC2
 
+sudo apt-get -qq update -y
+sudo apt-get -q install --no-install-recommends -y gcc make libc-dev wget
+
 # Install NVIDIA drivers 367.57
-#  This is incrementally newer than the version that we get updated to below (367.48) if we follow the instructions and install 361.42
-#  Hoping this works since 367.48 has been removed from the NVidia downloads site
+#  This is incrementally newer than the version 367.48 that the Ubuntu repos hold.
+#  NVidia no longer has the 367.48 version on their downloads site and we have
+#  to have the *.run file so that we match the host and the Docker container
 wget http://us.download.nvidia.com/XFree86/Linux-x86_64/367.57/NVIDIA-Linux-x86_64-367.57.run
-sudo apt-get -q install --no-install-recommends -y gcc make libc-dev
 sudo sh NVIDIA-Linux-x86_64-367.57.run --ui=none --no-questions --accept-license
