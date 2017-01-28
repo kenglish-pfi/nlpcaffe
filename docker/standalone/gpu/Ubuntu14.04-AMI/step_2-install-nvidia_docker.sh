1c78655d2d759bf23df48b27c18c550155a4b9d9
kenglish-pfi <kenglish.pfi@gmail.com>
2017-01-27 18:32:29 -0600

    A few more tweaks, GPU devices getting lost in the process ... still investigating

--- docker/standalone/gpu/Ubuntu14.04-AMI/step_2-install-nvidia_docker.sh
+++ docker/standalone/gpu/Ubuntu14.04-AMI/step_2-install-nvidia_docker.sh
@@ -14,6 +14,11 @@ curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
 wget https://apt.dockerproject.org/repo/pool/main/d/docker-engine/docker-engine_1.10.0-0~trusty_amd64.deb
 sudo dpkg -i docker-engine_1.10.0-0~trusty_amd64.deb
 
+sudo groupadd docker
+sudo gpasswd -a ${USER} docker
+sudo service docker restart
+
+
 # Install nvidia-docker and nvidia-docker-plugin
 wget https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.0-rc.3/nvidia-docker_1.0.0.rc.3-1_amd64.deb
 sudo dpkg -i nvidia-docker_1.0.0.rc.3-1_amd64.deb
